8a9285d5f649fa6b2655faed3b967b803249b1f5
Hector Osuna <55816095+FanGoH@users.noreply.github.com>
2021-08-23 09:29:17 -0700

    Fixed datatable float

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import "sweetalert/dist/sweetalert.css";
 import "ng-dialog/css/ngDialog.min.css";
 import "ng-dialog/css/ngDialog-theme-default.min.css";
 import "bootstrap";
+import "angular-datatables/dist/css/angular-datatables.min.css";
 
 export { globals } from "./js/config/envs/stg";
 // export { globals } from "./js/config/envs/stg";
