2753fc22c0cf344c10ba1eacfc184c5ef43d0ae7
despelair <52083283+DESPEL@users.noreply.github.com>
2022-01-03 18:33:17 -0800

    Directive to auto generate <select> based on a dictionary

---
+++ src/js/directives/form/select-object.js
@@ -0,0 +1,32 @@
+console.log("loaded ?")
+
+angular.module("formDirectives", [])
+  .directive("selectObject", function () {
+    return {
+      restrict: "E",
+      templateUrl: "partials/directives/form/select-object.html",
+      scope: {
+        data: "=data",
+        model: "=ngModel",
+        textProperty: "=textProperty",
+        valueProperty: "=valueProperty",
+        ifFunctionProperty: "=?ifFunctionProperty",
+        scope: "=?scope",
+      },
+      link: function ($scope) {
+        console.log("linking")
+        $scope.evalIf = (entry) => {
+          if (!$scope.ifFunctionProperty) {
+            return true
+          }
+
+          try {
+            return entry[$scope.ifFunctionProperty] ? entry[$scope.ifFunctionProperty](
+              $scope.scope) : true
+          } catch (e) {
+            return true
+          }
+        }
+      },
+    }
+  })
