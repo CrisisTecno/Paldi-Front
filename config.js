5488cf6c68ee5e0a6cc077813c4efe73dee4ea08
despelair <52083283+DESPEL@users.noreply.github.com>
2022-01-05 16:05:13 -0800

    fix windows path

--- config.js
+++ config.js
@@ -20,7 +20,7 @@ filesToDist = filesToDist.map((dir) => ({
 filesToDist = [{
 	from: "src/*/**/*.html",
 	to({ context, absoluteFilename }) {
-		const relPath = absoluteFilename.replace(context, "").replace("src", "dist").replace("/", "")
+		const relPath = absoluteFilename.replace(context, "").replace("src", "dist").replace("/", "").replace(/\\/g, "/")
 
 		console.log(relPath)
 		return path.resolve(__dirname, relPath)
