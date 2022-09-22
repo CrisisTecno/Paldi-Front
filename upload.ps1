5e303b5de294bb141b9a9aef80cd1d95d6e226f3
ManmadeArc <44855389+ManmadeArc@users.noreply.github.com>
2022-09-21 18:22:22 -0700

    Update upload.ps1

--- upload.ps1
+++ upload.ps1
@@ -1,10 +1,12 @@
 $ENV = Read-Host -Prompt 'Select PROD or STG'
 ssh-agent
 if ($ENV -eq 'PROD'){
-    scp -r ./dist root@cotizadorpaldi.com.mx:/var/www
-    ssh -t root@cotizadorpaldi.com.mx "cd /var/www && mv html html-$(Get-Date -UFormat "%m-%d-%Y-%T") && mv dist html"
+    npm run build-prod-internal
+    # scp -r ./dist root@cotizadorpaldi.com.mx:/var/www
+    # ssh -t root@cotizadorpaldi.com.mx "cd /var/www && mv html html-$(Get-Date -UFormat "%m-%d-%Y-%T") && mv dist html"
 }
 else{
-    scp -r ./dist root@192.241.238.176:/var/www
-    ssh -t root@192.241.238.176       "cd /var/www && mv html html-$(Get-Date -UFormat "%m-%d-%Y-%T") && mv dist html"
+    npm run build-stg-internal
+    # scp -r ./dist root@192.241.238.176:/var/www
+    # ssh -t root@192.241.238.176       "cd /var/www && mv html html-$(Get-Date -UFormat "%m-%d-%Y-%T") && mv dist html"
 }
